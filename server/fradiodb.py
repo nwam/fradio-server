8bff385be0594649e7abb4e7667a1d7f1c1d03f4
Noah Murad <noahmurad@hotmail.com>
2017-11-18 08:16:44 -0500

    Listner gets added to db on first request

--- server/fradiodb.py
+++ server/fradiodb.py
@@ -8,6 +8,14 @@ def query(q, args=None, cols=None):
 
     return query_response
 
+# Returns one row obtained from the query
+def query_all(q, args=None, cols=None):
+    cursor = MySQLdb.connect(user='root', db='fradio').cursor()
+    cursor.execute(q, args)
+    query_response = cursor.fetchall()
+
+    return query_response
+
 # Commits a query, returns number of rows changed
 def transact(q, args=None):
     connection = MySQLdb.connect(user='root', db='fradio')
