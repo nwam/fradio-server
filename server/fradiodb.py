4e65ae152987976623439520f14ca488a9ef3a96
Noah Murad <noahmurad@hotmail.com>
2017-11-20 02:27:04 -0500

    Remove self references

--- server/fradiodb.py
+++ server/fradiodb.py
@@ -38,25 +38,25 @@ def add_broadcast(username, track_id, start_time, scroll_time, trackLength, is_p
     broadcast = """INSERT INTO broadcast (spotifyUsername, spotifyTrackID, startTime, scrollTime, trackLength, isPlaying) \
                     VALUES(%s, %s, %s, %s, %s, %s)"""
     broadcast_args = (username, track_id, start_time, scroll_time, trackLength, is_playing)
-    return fradiodb.transact(broadcast, broadcast_args)
+    return transact(broadcast, broadcast_args)
 
 # Returns if username is found in user TABLE
 def user_exists(spotify_username):
     get_listener = """SELECT spotifyUsername FROM user WHERE spotifyUsername = %s"""
     get_listener_args = (spotify_username,)
-    listener = fradiodb.query(get_listener, get_listener_args)
+    listener = query(get_listener, get_listener_args)
     return listener != None
 
 def update_user(user, listening, ip_address):
     listen = """UPDATE user SET listening = %s, ipAddress = %s WHERE spotifyUsername = %s;"""
     listen_args = (listening, ip_address, user)
-    return fradiodb.transact(listen, listen_args)
+    return transact(listen, listen_args)
 
 def add_user(user, listening, ip_address):
     insert_listener = """INSERT INTO user (spotifyUsername, listening, ipAddress) \
                             VALUES (%s, %s, %s);"""
     insert_listener_args = (user, listening, ip_address)
-    return fradiodb.transact(insert_listener, insert_listener_args)
+    return transact(insert_listener, insert_listener_args)
 
 # Gets and formats a user's most recent broadcast
 def get_broadcast_json(username):
@@ -65,7 +65,7 @@ def get_broadcast_json(username):
                         WHERE broadcastID IN (SELECT MAX(broadcastID) FROM broadcast WHERE spotifyUsername = %s);"""
     get_broadcast_info_args = (username,)
     try:
-        spotify_track_id, start_time, scroll_time, track_length, broadcast_id, is_playing = fradiodb.query(get_broadcast_info, get_broadcast_info_args)
+        spotify_track_id, start_time, scroll_time, track_length, broadcast_id, is_playing = query(get_broadcast_info, get_broadcast_info_args)
     except:
         return json.dumps({'status':'Error: The requested user could not be found'})
 
@@ -90,14 +90,14 @@ def disconnect_user(user):
 def stop_listening(user):
     stop_listening = """UPDATE user SET listening = NULL WHERE spotifyUsername = %s;"""
     stop_listening_args = (user,)
-    return fradiodb.transact(stop_listening, stop_listening_args);
+    return transact(stop_listening, stop_listening_args);
 
 # Insert data which allows queries to determine streamer status
 def stop_streaming(user):
     stop_streaming = """INSERT INTO broadcast (spotifyUsername, isPlaying) \
                         VALUES (%s, %s);"""
     stop_streaming_args = (user, IS_STOPPED)
-    return fradiodb.transact(stop_streaming, stop_streaming_args);
+    return transact(stop_streaming, stop_streaming_args);
 
 # TODO: archive all broadcasts 
 # except most recent broadcast from each user
